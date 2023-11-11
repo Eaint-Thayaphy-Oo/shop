3451db14701d73c5bd874a977bc6efd8f19c4769
Eaint-Thayaphy-Oo <eaintthayaphyoo098@gmail.com>
2023-11-11 19:18:58 +0630

    login register commit

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/register', function () {
+    return view('register');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::middleware([
